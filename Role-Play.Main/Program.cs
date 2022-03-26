2e5f2e4015bc31130e42546eaed809ffd10ce8ce
Ryker Fisk <rrykersf@gmail.com>
2022-03-25 22:54:26 -0600

    Check-in 1

--- Role-Play.Main/Program.cs
+++ Role-Play.Main/Program.cs
@@ -8,7 +8,27 @@ namespace Role_Play.Main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Role-Playing Assistant! With this program you can create a character, save them to the program, and load them for later use! You can also use this program to roll dice!");
+            Console.WriteLine("Enter the following based on what you want to do:");
+            Console.WriteLine("Enter '1' to create a character.");
+            Console.WriteLine("Enter '2' to view and load the stats of your saved characters.");
+            Console.WriteLine("Enter '3' to simulate rolling dice.");
+            Console.ReadLine();
         }
     }
 }
+
+/*Expected workflow for testing:
+Go to the character creation options to create and save a new character
+Make sure the character file was properly written to and can be loaded again
+Go to the terminal that will allow me to roll dice and make sure it works properly
+
+In more detail: 
+    Enter 1 to get to character creation screen, fill in all required
+so that the program can save that character to a text file. Exit.
+    Enter 2 from the main screen to enter the screen that will display the names
+of all saved characters. Upon typing in the name of your character, it will pull
+up the stats of that character. Exit.
+    Enter 3 to enter the dice rolling screen where the user can choose how
+many and what kinds of dice they are rolling. If it works correctly, exit.
+*/
--- Role-Play/Class1.cs
+++ Role-Play/Class1.cs
@@ -2,9 +2,122 @@
 
 //Logic
 
+//Next step: Dice
+//
+
 namespace Role_Play
 {
+    //For rolling Dice
     public class Class1
     {
+        static int ValidateInt(string prompt, int min = 0, int max = int.MaxValue)
+        {
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                try
+                {
+                    var intInput = int.Parse(Console.ReadLine());
+                    if (intInput <= max && intInput >= min)
+                    {
+                        return intInput;
+                    }
+                    else if (intInput > max || intInput < min)
+                    {
+                        Console.WriteLine($"\nThat isn't within your options between {min} and {max}.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("That didn't work, try using one of the suggested options.");
+                }
+            }
+        }
+        public class Dice
+        {
+            public interface IDice
+            {
+                int numSides();
+                void numRolls();
+            }
+
+            public class D4 : IDice
+            {
+                public int numSides()
+                {
+                    return 4;
+                }
+                public void numRolls()
+                {
+                    //Will roll these dice as many times as dictated by the user from the main project
+                    //Code pending
+                }
+            }
+
+            public class D6 : IDice
+            {
+                public int numSides()
+                {
+                    return 6;
+                }
+                public void numRolls()
+                {
+                    //Will roll these dice as many times as dictated by the user from the main project
+                    //Code pending
+                }
+            }
+
+            public class D8 : IDice
+            {
+                public int numSides()
+                {
+                    return 8;
+                }
+                public void numRolls()
+                {
+                    //Will roll these dice as many times as dictated by the user from the main project
+                    //Code pending
+                }
+            }
+
+            public class D10 : IDice
+            {
+                public int numSides()
+                {
+                    return 10;
+                }
+                public void numRolls()
+                {
+                    //Will roll these dice as many times as dictated by the user from the main project
+                    //Code pending
+                }
+            }
+
+            public class D12 : IDice
+            {
+                public int numSides()
+                {
+                    return 12;
+                }
+                public void numRolls()
+                {
+                    //Will roll these dice as many times as dictated by the user from the main project
+                    //Code pending
+                }
+            }
+
+            public class D20 : IDice
+            {
+                public int numSides()
+                {
+                    return 20;
+                }
+                public void numRolls()
+                {
+                    //Will roll these dice as many times as dictated by the user from the main project
+                    //Code pending
+                }
+            }
+        }
     }
 }
